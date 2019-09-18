b4ef11aeb64c14703d72dc91175d6de93089fd42
alexiscash <alexiscash97@rocketmail.com>
2019-09-18 10:02:05 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    if self.include?('.')
+      sentence = self.split('. ')
+      count = sentence.map do |elem|
+        elem.split('! ')
+      end
+      count.flatten!
+      count = count.map do |elem|
+        elem.split('? ')
+      end
+      count.flatten!.size
+    else
+      return 0
+    end
   end
 end
+
+p "This, well, is a sentence. This is too!! And so is this, I think? Woo...".split(/\.|\?|\!/)
